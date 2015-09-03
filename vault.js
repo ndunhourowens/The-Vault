6ecd684a369a65c37c9bebc48daef8f00a5654e8
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-02 20:12:14 -1000

    commit finished product.  Still lostgit status

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
+
 'use strict';
 module.exports = function() {
-  
+  var vault = {};
+
+  function _setValue ( key, value) {
+    vault[key] = value;
+  }
+
+  function _getValue ( key) {
+    if ( vault.hasOwnProperty(key)) {
+      return vault[key];
+    }else {
+      return null;
+    }
+  }
+
+return {
+  setValue : _setValue,
+  getValue : _getValue
 };
+};
+
